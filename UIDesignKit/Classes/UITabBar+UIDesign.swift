3a10891d8403abaab744cd12b46deeab6fbabf76
Will Powell <willpowell8@gmail.com>
2017-08-07 23:30:43 +0100

    Update to add UITabBar Tint

---
+++ UIDesignKit/Classes/UITabBar+UIDesign.swift
@@ -0,0 +1,29 @@
+//
+//  UITabBar+UIDesign.swift
+//  Pods
+//
+//  Created by Will Powell on 07/08/2017.
+//
+//
+
+import Foundation
+extension UITabBar{
+    override public func updateDesign(type:String, data:[AnyHashable: Any]) {
+        super.updateDesign(type:type, data: data);
+        self.applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
+            if let v = value as? UIColor {
+                self.tintColor = v
+            }
+        })
+        
+    }
+    override public func getDesignProperties(data:[String:Any]) -> [String:Any]{
+        var dataReturn = super.getDesignProperties(data: data);
+        dataReturn["tintColor"] = ["type":"COLOR", "value":self.tintColor.toHexString()];
+        return dataReturn;
+    }
+    
+    override public func getDesignType() -> String{
+        return "TABBAR";
+    }
+}
