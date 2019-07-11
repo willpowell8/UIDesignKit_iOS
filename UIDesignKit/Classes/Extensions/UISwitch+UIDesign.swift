0b97be9b45b8204816fb4d4c549390c3ca1883c8
Will Powell <willpowell8@gmail.com>
2019-07-11 09:53:22 +0100

    update of tint colour and handllers

--- UIDesignKit/Classes/Extensions/UISwitch+UIDesign.swift
+++ UIDesignKit/Classes/Extensions/UISwitch+UIDesign.swift
@@ -9,15 +9,21 @@ import Foundation
 extension UISwitch{
     override open func updateDesign(type:String, data:[AnyHashable: Any]) {
         super.updateDesign(type:type, data: data);
-        self.applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
+        self.applyData(data: data, property: "onTintColor", targetType: .color, apply: { (value) in
             if let v = value as? UIColor {
-                self.tintColor = v
+                self.onTintColor = v
+            }
+        })
+        self.applyData(data: data, property: "thumbTintColor", targetType: .color, apply: { (value) in
+            if let v = value as? UIColor {
+                self.thumbTintColor = v
             }
         })
     }
     override open func getDesignProperties(data:[String:Any]) -> [String:Any]{
         var dataReturn = super.getDesignProperties(data: data);
-        dataReturn["tintColor"] = ["type":"COLOR", "value":self.tintColor.toHexString()];
+        dataReturn["onTintColor"] = ["type":"COLOR", "value":self.onTintColor?.toHexString()];
+        dataReturn["thumbTintColor"] = ["type":"COLOR", "value":self.thumbTintColor?.toHexString()];
         return dataReturn;
     }
     
