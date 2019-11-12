1a7a10b0fbc043cefb6ef0e6c26d275bc1110c32
Will Powell <willpowell8@gmail.com>
2019-11-12 13:30:23 +0000

    update to 5.0.13 to remove uisearchbar dependency

--- UIDesignKit.podspec
+++ UIDesignKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'UIDesignKit'
-  s.version          = '5.0.12'
+  s.version          = '5.0.13'
   s.summary          = 'Remotely tweak and change colours, fonts, style and design parameters without recompiling your app.'
 
 # This description is used to generate tags and improve search results.
--- UIDesignKit/Classes/Extensions/UISearchBar+UIDesign.swift
+++ UIDesignKit/Classes/Extensions/UISearchBar+UIDesign.swift
@@ -10,28 +10,29 @@ import Foundation
 extension UISearchBar{
     override open func updateDesign(type:String, data:[AnyHashable: Any]) {
         super.updateDesign(type:type, data: data);
-        self.applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
+        applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
             if let v = value as? UIColor {
                 self.tintColor = v
             }
         })
-        self.applyData(data: data, property: "barTintColor", targetType: .color, apply: { (value) in
+        applyData(data: data, property: "barTintColor", targetType: .color, apply: { (value) in
             if let v = value as? UIColor {
                 self.barTintColor = v
             }
         })
         
-        
-        self.applyData(data: data, property: "searchBarTextColor", targetType: .color) { (value) in
-            if let v = value as? UIColor {
-                self.searchTextField.textColor = v
-                self.searchTextField.leftView?.tintColor = v
+        if #available(iOS 13.0, *) {
+            applyData(data: data, property: "searchBarTextColor", targetType: .color) { (value) in
+                if let v = value as? UIColor {
+                    self.searchTextField.textColor = v
+                    self.searchTextField.leftView?.tintColor = v
+                }
             }
-        }
-        
-        self.applyData(data: data, property: "searchBarBackgroundColor", targetType: .color) { (value) in
-            if let v = value as? UIColor {
-                self.searchTextField.backgroundColor = v
+            
+            applyData(data: data, property: "searchBarBackgroundColor", targetType: .color) { (value) in
+                if let v = value as? UIColor {
+                    self.searchTextField.backgroundColor = v
+                }
             }
         }
         
@@ -46,10 +47,12 @@ extension UISearchBar{
         }else{
             dataReturn["tintColor"] = ["type":"COLOR", "value":self.tintColor.toHexString()]
         }
-        let searchTextFieldHex = (searchTextField.textColor ?? UIColor.black).toHexString()
-        let searchTextFieldBGHex = (searchTextField.backgroundColor ?? UIColor.white).toHexString()
-        dataReturn["searchBarTextColor"] = ["type":"COLOR", "value":searchTextFieldHex]
-        dataReturn["searchBarBackgroundColor"] = ["type":"COLOR", "value":searchTextFieldBGHex]
+        if #available(iOS 13.0, *) {
+            let searchTextFieldHex = (searchTextField.textColor ?? UIColor.black).toHexString()
+            let searchTextFieldBGHex = (searchTextField.backgroundColor ?? UIColor.white).toHexString()
+            dataReturn["searchBarTextColor"] = ["type":"COLOR", "value":searchTextFieldHex]
+            dataReturn["searchBarBackgroundColor"] = ["type":"COLOR", "value":searchTextFieldBGHex]
+        }
         if let barTintColor = self.barTintColor {
             if #available(iOS 13.0, *) {
                 let lightColor = colorForTrait(color: barTintColor, trait: .light)
