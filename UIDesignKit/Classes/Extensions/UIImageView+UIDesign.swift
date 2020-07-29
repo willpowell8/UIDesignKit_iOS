e5b2346febfdcb854886a8e950785adfc98d4ef5
Will Powell <will.powell@keytree.co.uk>
2020-07-29 14:04:24 +0100

    udpate to design tints on images

--- UIDesignKit/Classes/Extensions/UIImageView+UIDesign.swift
+++ UIDesignKit/Classes/Extensions/UIImageView+UIDesign.swift
@@ -23,11 +23,27 @@ extension UIImageView{
                 self.contentMode = contentModeVal
             }
         })
+        
+        self.applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
+            if let v = value as? UIColor {
+                self.tintColor = v
+            }
+        })
     }
     override open func getDesignProperties(data:[String:Any]) -> [String:Any]{
         var dataReturn = super.getDesignProperties(data: data);
         dataReturn["url"] = ["type":"URL"];
         dataReturn["contentMode"] = ["type":"INT", "value": self.contentMode.rawValue];
+        
+        if #available(iOS 13.0, *) {
+            let lightColor = colorForTrait(color: self.tintColor, trait: .light)
+            let darkColor = colorForTrait(color: self.tintColor, trait: .dark)
+            dataReturn["tintColor"] = ["type":"COLOR", "value":lightColor?.toHexString()]
+            dataReturn["tintColor-dark"] = ["type":"COLOR", "value":darkColor?.toHexString()]
+        }else{
+            dataReturn["tintColor"] = ["type":"COLOR", "value":self.tintColor.toHexString()]
+        }
+        
         return dataReturn;
     }
     
