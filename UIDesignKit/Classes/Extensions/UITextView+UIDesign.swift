1532ab736861e0290748bada177d46d3d4bbfe0e
Will Powell <will.powell@keytree.co.uk>
2020-08-04 14:15:04 +0100

    update to 5.0.21

--- UIDesignKit/Classes/Extensions/UITextView+UIDesign.swift
+++ UIDesignKit/Classes/Extensions/UITextView+UIDesign.swift
@@ -24,9 +24,23 @@ extension UITextView{
             }
             self.font = f
         })
+        
+        self.applyData(data: data, property: "tintColor", targetType: .color, apply: { (value) in
+            if let v = value as? UIColor {
+                self.tintColor = v
+            }
+        })
     }
     override open func getDesignProperties(data:[String:Any]) -> [String:Any]{
         var dataReturn = super.getDesignProperties(data: data)
+        if #available(iOS 13.0, *) {
+            let lightColor = colorForTrait(color: self.tintColor, trait: .light)
+            let darkColor = colorForTrait(color: self.tintColor, trait: .dark)
+            dataReturn["tintColor"] = ["type":"COLOR", "value":lightColor?.toHexString()]
+            dataReturn["tintColor-dark"] = ["type":"COLOR", "value":darkColor?.toHexString()]
+        }else{
+            dataReturn["tintColor"] = ["type":"COLOR", "value":self.tintColor.toHexString()]
+        }
         
         if #available(iOS 13.0, *) {
             let lightColor = colorForTrait(color: self.textColor, trait: .light)
